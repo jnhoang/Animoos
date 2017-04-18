a6c84528c84d6d670bde23b304a11933e40e1259
jnhustin <jnhustin@gmail.com>
2017-04-17 20:40:42 -0700

    added modal for character info

--- public/app/controllers/anime-page-controller.js
+++ public/app/controllers/anime-page-controller.js
@@ -6,9 +6,16 @@ angular
 , '$stateParams'
 , 'AnimeAPIFactory'
 , function($scope, $state, $stateParams, AnimeAPIFactory) {
+    
+    // PUBLIC VARS & FUNCTIONS 
     $scope.animeData;
+    $scope.charData;
     $scope.loading = true;
+    $scope.loadingModal = true;
     $scope.errorMessage;
+
+    $scope.getCharById = getCharById;
+
 
     AnimeAPIFactory.getAnimeById($stateParams.id)
     .then(function(res) {
@@ -23,12 +30,17 @@ angular
     })
 
 
-    AnimeAPIFactory.getCharById(1)
-    .then(function(res) {
-      console.log(res.data);
-    })
-    .catch(function(err) {
-      console.log(err.message[0]);
-    })
+    function getCharById(id) {
+      $scope.loadingModal = true;
+
+      AnimeAPIFactory.getCharById(id)
+      .then(function(res) {
+        console.log(res.data);
+        $scope.loadingModal = false;
+        $scope.charData = res.data;
+      })
+      .catch(function(err) { console.log(err.message[0]); });
+    }
+
   }
 ])
