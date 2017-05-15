e9491a2c7dc7e1d6234ad7e4dc0e732790b7c783
jnhustin <jnhustin@gmail.com>
2017-05-14 21:23:15 -0700

    apifactory, added comment documentation & prepped remaining functions for promise update

--- public/app/factories/anime-api-factory.js
+++ public/app/factories/anime-api-factory.js
@@ -6,7 +6,10 @@ angular
 , '$log'
 , function($http, $q, $log) {
     var storage = {
-      charData: {}
+      animeData:    {}
+    , browseData:   {}
+    , charData:     {}
+    , searchData:   {}
     };
 
     return {
@@ -16,37 +19,39 @@ angular
     , browseBy: browseBy
     }
   
-  function browseBy(filter) {
-    return $http({
-      url: 'api/anilist/browse'
-    , method: 'GET'
-    , params: filter
-    });
-  }
-  function getCharById(id) {
-    var deferred = $q.defer();
-
-    if (storage.charData[id]) {
-      $log.debug('id stored in cache', storage.charData[id]);
-      deferred.resolve(storage.charData[id]);
+    function browseBy(filter) {
+      return $http({
+        url: 'api/anilist/browse'
+      , method: 'GET'
+      , params: filter
+      });
     }
-    else {
-      $http.get('/api/anilist/page-data/character/' + id)
-      .then(function(res) { 
-        storage.charData[id] = res.data;
-        $log.debug('successfully fetched data from API for char id: ', id);
+    // returns factory storage || api call for char data via a promise
+    function getCharById(id) {
+      var deferred = $q.defer();
+      if (storage.charData[id]) {
+        // returns local data if available
+        $log.debug('id stored in cache', storage.charData[id]);
         deferred.resolve(storage.charData[id]);
-      })
-      .catch(function(err) { console.log(err.message); });
+      }
+      else {
+        // api call for data, stores in local cache, returns local data
+        $http.get('/api/anilist/page-data/character/' + id)
+        .then(function(res) { 
+          storage.charData[id] = res.data;
+          $log.debug('successfully fetched data from API for char id: ', id);
+          deferred.resolve(storage.charData[id]);
+        })
+        .catch(function(err) { defer.reject(err) });
+      }
+      // returns promise
+      return deferred.promise;
+    } 
+    function getAnimeById(id) {
+      return $http.get('/api/anilist/page-data/anime/' + id);
     }
-    return deferred.promise;
-
-
-  } 
-  function getAnimeById(id) {
-    return $http.get('/api/anilist/page-data/anime/' + id);
+    function searchForAnime(animeTitle) {
+      return $http.get('/api/anilist/search/anime/' + animeTitle);
+    }
   }
-  function searchForAnime(animeTitle) {
-    return $http.get('/api/anilist/search/anime/' + animeTitle);
-  }
-}])
+]);
