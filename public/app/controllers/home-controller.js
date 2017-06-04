a2c8d63e17fbece701194a54905016479be581ea
jnhustin <jnhustin@gmail.com>
2017-06-03 20:03:11 -0700

    minor refactor, home controller: .watch function

--- public/app/controllers/home-controller.js
+++ public/app/controllers/home-controller.js
@@ -12,19 +12,37 @@ angular
     $scope.loading      = true;
     $scope.loadingBar   = false;
 
+    // ADVANCE FILTER ASSETS
+
+    $scope.genres = [
+      'Genres', 'action', 'adventure', 'comedy', 'drama', 'fantasy', 'horror',
+      'mahou shoujo', 'mecha', 'music', 'mystery', 'psychological', 'romance', 
+      'sci fi', 'slice of life', 'sports'
+    ];
+    // Initial API call
+    $scope.filterObj = {
+      token_type: 'Bearer'
+    , year:       ''
+    , season:     ''
+    , type:       ''
+    , status:     ''
+    , genres:     []
+    , sort:       'popularity-desc'
+    , page:       1
+    }
+
     // auto API call on any change to filters
     $scope.$watchCollection('filterObj', function(newObj, oldObj) {
+      // prevents premature API calls
       if ($scope.filterObj.year && $scope.filterObj.year.length != 4) {
         return;
       }
-
+      // notify user search in progress
       $scope.loadingBar = true;
-      // clears empty fields from filterObj
-      for (var key in $scope.filterObj) {
-        if ($scope.filterObj[key] == '') {
-          delete $scope.filterObj[key]
-        }
-      }
+      
+      // clears empty fields from $scope.filterObj
+      clearEmptyFields();
+
       AnimeAPIFactory.browseBy($scope.filterObj)
       .then(function(res) {
         $scope.showArr      = res.data;
@@ -47,32 +65,19 @@ angular
           Reload the page or try again later', 10000);
       });
     });
-
+    function clearEmptyFields() {
+      for (var key in $scope.filterObj) {
+        if ($scope.filterObj[key] == '') {
+          delete $scope.filterObj[key]
+        }
+      }
+    }
     function adjustApiData(animeArr) {
       animeArr.forEach(function(anime) {
         for (var i = 0; i < anime.genres.length - 1; i++) {
           anime.genres[i] += ',';
         }
       });
-    }
-    
-    // ADVANCE FILTER ASSETS
-
-    $scope.genres = [
-      'Genres', 'action', 'adventure', 'comedy', 'drama', 'fantasy', 'horror',
-      'mahou shoujo', 'mecha', 'music', 'mystery', 'psychological', 'romance', 
-      'sci fi', 'slice of life', 'sports'
-    ];
-    // Initial API call
-    $scope.filterObj = {
-      token_type: 'Bearer'
-    , year:       ''
-    , season:     ''
-    , type:       ''
-    , status:     ''
-    , genres:     []
-    , sort:       'popularity-desc'
-    , page:       1
     }
 
   }
